cf6b3903cea2cac29dab326a1bda5039f0cad82e
metincansiper <metincansiper@gmail.com>
2017-09-15 21:40:55 -0700

    Adopt tests for modal manager from master branch

--- public/test/testsUserOperations.js
+++ public/test/testsUserOperations.js
@@ -1,5 +1,5 @@
 QUnit = require('qunitjs');
-module.exports = function(){
+module.exports = function(modelManager){
   QUnit.module( "User operations tests to see if model is updated correctly." );
 
   function addNodeTest(id, className, posX, posY) {
@@ -10,6 +10,14 @@ module.exports = function(){
       assert.equal(node.position('x'), posX, 'x position of the node is as expected');
       assert.equal(node.position('y'), posY, 'y position of the node is as expected');
       assert.equal(node.data('class'), className, 'the node has the expected sbgn class');
+
+      var nodeModel = modelManager.getModelNode(id);
+
+      assert.ok(nodeModel, "Node is added to the model.");
+      assert.equal(modelManager.getModelNodeAttribute("data.id", id), cy.getElementById(id).data("id") , "Node is equal in model and cytoscape");
+      assert.equal(node.data("class"), modelManager.getModelNodeAttribute("data.class", id), "Node class is equal in model and cytoscape.");
+      assert.equal(node.position('x'),modelManager.getModelNodeAttribute("position.x", id), "Node position x is equal in model and cytoscape.");
+      assert.equal(node.position('y'),modelManager.getModelNodeAttribute("position.y", id), "Node position y is equal in model and cytoscape.");
     });
   }
 
@@ -21,6 +29,14 @@ module.exports = function(){
       assert.ok(edge, "An edge with id: " + id + " is added.");
       assert.equal(edge.data('source'), src, "the edge has the expected source");
       assert.equal(edge.data('target'), tgt, "the edge has the expected target");
+
+      var edgeModel = modelManager.getModelEdge(id);
+
+      assert.ok(edgeModel, "Edge is added to the model.");
+      assert.equal(modelManager.getModelEdgeAttribute("data.id", id), cy.getElementById(id).data("id") , "Edge is equal in model and cytoscape");
+      assert.equal(edge.data("class"), modelManager.getModelEdgeAttribute("data.class", id), "Edge class is equal in model and cytoscape.");
+      assert.equal(edge.data('source'),modelManager.getModelEdgeAttribute("data.source", id), "Edge target x is equal in model and cytoscape.");
+      assert.equal(edge.data('target'),modelManager.getModelEdgeAttribute("data.target", id), "Edge source y is equal in model and cytoscape.");
     });
   }
 
@@ -37,6 +53,12 @@ module.exports = function(){
       // assert.equal(cy.nodes('#anotherMacromolecule').parent().data('class'), 'complex', 'Create compound operation is successful node');
       assert.equal(newCompound.id(), cy.getElementById(id).data('parent'), "Compound is created");
       assert.equal(newCompound.data('class'), compoundType, "Compound has the correct sbgn class");
+
+      var compoundModel = modelManager.getModelNode(newCompound.id());
+
+      assert.ok(compoundModel, "Compound is added to the model.");
+      assert.equal(modelManager.getModelNodeAttribute("data.id", newCompound.id()), cy.getElementById(id).data("parent"), "Compound is the parent of the node.");
+      assert.equal(modelManager.getModelNodeAttribute("data.class", newCompound.id()), compoundType, "Model compound has the correct sbgn class.");
     });
   }
 
@@ -61,13 +83,36 @@ module.exports = function(){
       assert.equal(filteredNodes.children().length, initilChildrenSize, "Initial children size is protected after expand operation");
       assert.equal(cy.nodes().length, initialNodesSize, "Initial nodes size is protected after expand operation");
       assert.equal(cy.edges().length, initialEdgesSize, "Initial edges size is protected after expand operation");
+
+      filteredNodes.forEach(function(node){
+        var expandCollapseStatus = modelManager.getModelNodeAttribute('expandCollapseStatus', node.id());
+        assert.equal(expandCollapseStatus, "expand", "Expand on node " + node.id()  + " is successful");
+      });
     });
   }
 
   function deleteElesTest(selector) {
     QUnit.test('chise.deleteElesSimple()', function (assert) {
+      var nodeIds = [];
+      var edgeIds = [];
+
+      cy.elements(selector).forEach(function(ele){
+          if(ele.isNode())
+              nodeIds.push(ele.id());
+          else
+              edgeIds.push(ele.id());
+      });
+
       chise.deleteElesSimple(cy.elements(selector));
       assert.equal(cy.elements(selector).length, 0, "Delete simple operation is successful");
+
+      nodeIds.forEach(function(nodeId){
+        assert.notOk(modelManager.getModelNode(nodeId), "Node " + nodeId + " removed successfully.")
+      });
+
+      edgeIds.forEach(function(edgeId){
+        assert.notOk(modelManager.getModelEdge(edgeId), "Edge " + edgeId + " removed successfully.")
+      });
     });
   }
 
@@ -79,6 +124,11 @@ module.exports = function(){
 
       chise.hideNodesSmart(nodes);
       assert.equal(cy.nodes().filter(':visible').length, nodesToShow.length, "Hide operation is successful");
+
+      nodes.forEach(function(node){
+        var visibilityStatus = modelManager.getModelNodeAttribute('visibilityStatus', node.id());
+        assert.equal(visibilityStatus, "hide", "Hide on node " + node.id()  + " is successful");
+      });
     });
   }
 
@@ -86,6 +136,11 @@ module.exports = function(){
     QUnit.test('chise.showAll()', function (assert) {
       chise.showAll();
       assert.equal(cy.nodes().length, cy.nodes(':visible').length, "Show all operation is successful");
+
+      cy.nodes().forEach(function(node){
+        var visibilityStatus = modelManager.getModelNodeAttribute('visibilityStatus', node.id());
+        assert.equal(visibilityStatus, "show", "Show on node " + node.id()  + " is successful");
+      });
     });
   }
 
@@ -95,6 +150,17 @@ module.exports = function(){
       var elesToHighlight = chise.elementUtilities.getNeighboursOfNodes(node);
       chise.highlightNeighbours(node);
       assert.equal(elesToHighlight.filter('.highlighted').length, elesToHighlight.length, "Highlight neighbours operation is successful");
+
+      elesToHighlight.forEach(function(ele){
+        if(ele.isNode()){
+          var highlightStatus = modelManager.getModelNodeAttribute('highlightStatus', ele.id());
+          assert.equal(highlightStatus, "highlighted", "Highlight on node " + ele.id()  + " is successful");
+        }
+        else{
+          var highlightStatus = modelManager.getModelEdgeAttribute('highlightStatus', node.id());
+          assert.equal(highlightStatus, "highlighted", "Highlight on edge " + ele.id()  + " is successful");
+        }
+      });
     });
   }
 
@@ -102,6 +168,17 @@ module.exports = function(){
     QUnit.test('chise.removeHighlights()', function (assert) {
       chise.removeHighlights();
       assert.equal(cy.elements('.highlighted').length, 0, "Remove highlights operation is successful");
+
+      cy.elements().forEach(function(ele){
+        if(ele.isNode()){
+          var highlightStatus = modelManager.getModelNodeAttribute('highlightStatus', ele.id());
+          assert.equal(highlightStatus, "unhighlighted", "Unhighlight on node " + ele.id()  + " is successful");
+        }
+        else{
+          var highlightStatus = modelManager.getModelEdgeAttribute('highlightStatus', node.id());
+          assert.equal(highlightStatus, "unhighlighted", "Unhighlight on edge " + ele.id()  + " is successful");
+        }
+      });
     });
   }
 
@@ -111,6 +188,17 @@ module.exports = function(){
       var elesToHighlight = chise.elementUtilities.extendNodeList(nodes);
       chise.highlightProcesses(nodes);
       assert.equal(elesToHighlight.filter('.highlighted').length, elesToHighlight.length, "Highlight processes operation is successful");
+
+      elesToHighlight.forEach(function(ele){
+        if(ele.isNode()){
+          var highlightStatus = modelManager.getModelNodeAttribute('highlightStatus', ele.id());
+          assert.equal(highlightStatus, "highlighted", "Highlight on node " + ele.id()  + " is successful");
+        }
+        else{
+          var highlightStatus = modelManager.getModelEdgeAttribute('highlightStatus', node.id());
+          assert.equal(highlightStatus, "highlighted", "Highlight on edge " + ele.id()  + " is successful");
+        }
+      });
     });
   }
 
@@ -119,6 +207,11 @@ module.exports = function(){
       var nodes = cy.nodes(selector);
       chise.changeNodeLabel(nodes, 'test label');
       assert.equal(nodes.filter('[label="test label"]').length, nodes.length, "Change node label operation is successful");
+
+      nodes.forEach(function(node){
+        var newNodeLabel = modelManager.getModelNodeAttribute('data.label', node.id());
+        assert.equal(newNodeLabel, 'test label',  "Change node label operation is successful");
+      });
     });
   }
 
@@ -137,6 +230,14 @@ module.exports = function(){
         return node.data('bbox')[dimension] === 100;
       });
       assert.equal(filteredNodes.length, nodes.length, "Change " + dimension + " operation is successful");
+
+      filteredNodes.forEach(function(node){
+        var newNodeDimension = modelManager.getModelNodeAttribute('data.bbox', node.id());
+        console.log(newNodeDimension[dimension]);
+        console.log(node.data('bbox')[dimension]);
+
+        assert.equal(newNodeDimension[dimension], node.data('bbox')[dimension],  "Change " + dimension + " operation is successful");
+      });
     });
   }
 
@@ -163,6 +264,12 @@ module.exports = function(){
 
         assert.equal(dataUpdated.length, nodes.length, "Change " + name + " operation is successfully changed element data");
         assert.equal(styleUpdated.length, nodes.length, "Change " + name + " operation is successfully changed element style");
+
+        nodes.forEach(function(node){
+          var attStr = 'data.' + name;
+          var attVal = modelManager.getModelNodeAttribute(attStr, node.id());
+          assert.equal(attVal, testVal, "Change " + name + " operation is successful in the model.");
+        });
       });
   }
 
